7fce0529589ac2a1e3cc7071d4f67c137f73e178
apc7788 <albert.pc@hotmail.es>
2022-02-11 12:05:46 +0100

    Primer commit

---
+++ api-rest-spring-compras/src/main/java/com/formacionjava/app/apirest/ApiRestSpring1Application.java
@@ -0,0 +1,13 @@
+package com.formacionjava.app.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpring1Application.class, args);
+	}
+
+}
---
+++ api-rest-spring-compras/src/test/java/com/formacionjava/app/apirest/ApiRestSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionjava.app.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
