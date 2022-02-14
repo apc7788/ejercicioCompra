5691c3335aee6b0e8c2019c550e8278dd236d83b
Catalin Petrehus <59731303+Catatroski@users.noreply.github.com>
2022-02-14 10:53:25 +0100

    Cata resources

---
+++ resources/import.sql
@@ -0,0 +1,18 @@
+INSERT INTO clientes(nombre, apellido, empresa, puesto, cp, provincia, telefono, fecha_nacimiento) VALUES ("Catalin", "Petrehus", "dsad", "programador",43243, "rqweqew", 2362346, "2000-01-12");
+INSERT INTO clientes(nombre, apellido, empresa, puesto, cp, provincia, telefono, fecha_nacimiento) VALUES ("Juan", "Jose", "dsafa", "programador", 43243, "Marqeweqredrid", 432748, "2000-01-12");
+INSERT INTO clientes(nombre, apellido, empresa, puesto, cp, provincia, telefono, fecha_nacimiento) VALUES ("Pedro", "Sanchez", "wefwe", "programador", 43243, "Madrewrid", 284238, "2000-01-12");
+INSERT INTO clientes(nombre, apellido, empresa, puesto, cp, provincia, telefono, fecha_nacimiento) VALUES ("Pablo", "Motos", "eqwrqe", "programador", 43243, "Madqwqwrid", 1288524, "2000-01-12");
+INSERT INTO clientes(nombre, apellido, empresa, puesto, cp, provincia, telefono, fecha_nacimiento) VALUES ("Pepe", "Guarnizo", "rqerq", "programador", 43243, "Maderwwerrid", 945685, "2000-01-12");
+
+
+INSERT INTO articulos(nombre, descripcion, precioU, unidades, stock_seguridad) VALUES ("Jabon", "Bonito", 50, 200, 100);
+INSERT INTO articulos(nombre, descripcion, precioU, unidades, stock_seguridad) VALUES ("Peine", "mu Bonito", 213, 321, 100);
+INSERT INTO articulos(nombre, descripcion, precioU, unidades, stock_seguridad) VALUES ("Mesa", "feo", 23, 432, 100);
+INSERT INTO articulos(nombre, descripcion, precioU, unidades, stock_seguridad) VALUES ("Lampara", "luminoso", 5123, 213, 100);
+INSERT INTO articulos(nombre, descripcion, precioU, unidades, stock_seguridad) VALUES ("Sofa", "comodo", 2, 345, 100);
+
+
+INSERT INTO compras (cod_articulo, fecha, unidades) VALUES (4, "25-10-2017", 50);
+INSERT INTO compras (cod_articulo, fecha, unidades) VALUES (3, "23-03-2017", 100);
+INSERT INTO compras (cod_articulo, fecha, unidades) VALUES (2, "12-06-2017", 4);
+INSERT INTO compras (cod_articulo, fecha, unidades) VALUES (1, "16-12-2017", 42);
